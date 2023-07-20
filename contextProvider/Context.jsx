7da6e6e50d484f925052b4214815c274889265e5
Amy <86963506+aeposten@users.noreply.github.com>
2023-07-20 07:34:46 -0700

    Updated access-control-origin

--- contextProvider/Context.jsx
+++ contextProvider/Context.jsx
@@ -1,76 +1,86 @@
-import React, { useState } from 'react'
-import channelData from '../utils/channelData'
-const Context = React.createContext()
-const URL = 'https://tangerine-torte-2fd1e5.netlify.app/.netlify/functions/fetch-videos'
-const KEY = 'AIzaSyC-0bMXoBYRfR1Gp5k1JCVNy38cgIX_IHk'
-function ContextProvider({children}) {
-    const [channelId, setChannelId] = useState(channelData[0].id)
-    const [searchValue, setSearchValue] = useState('')
-    const [videosData, setVideosData] = useState([])
-    const [errorMessage, setErrorMessage] = useState('')
-    const [hasData, setHasData] = useState(false)
-        
-    const buttonClass = searchValue ? 'enabled' : 'disabled'
-        
-    function handleSelectChange(e) {
-        setChannelId(e.target.value)
-    }
-        
-    function handleSearchFieldChange(e) {
-        setSearchValue(e.target.value)
-    }
-        
-    async function searchForVideos(e) {
-        let conversationStr = ''
-        e.preventDefault()
-        const response  = await fetch(URL, {
-            method: 'POST',
-            headers: {
-                'content-type': 'text/plain',
-                'Access-Control-Allow-Origin': '*',
-            },
-            body: conversationStr,
-        })
+import React, { useState } from "react";
+import channelData from "../utils/channelData";
+const Context = React.createContext();
+const URL =
+  "https://tangerine-torte-2fd1e5.netlify.app/.netlify/functions/fetch-videos";
+const KEY = "AIzaSyC-0bMXoBYRfR1Gp5k1JCVNy38cgIX_IHk";
+function ContextProvider({ children }) {
+  const [channelId, setChannelId] = useState(channelData[0].id);
+  const [searchValue, setSearchValue] = useState("");
+  const [videosData, setVideosData] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [hasData, setHasData] = useState(false);
 
-        const data = await response.json();
-        console.log(data)
-        // fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&key=${KEY}&maxResults=10&channelId=${channelId}&q=${searchValue}`)
-        //     .then(res => {
-        //         if (!res.ok) {
-        //             setHasData(false)
-        //             console.log('res.ok === false')
-        //             setErrorMessage('The search engine is currently unavailable. Please try again later.')
-        //         }
-        //         else {
-        //             return res.json()
-        //         }
-        //     })
-        //     .then(data => {
-        //         if (!data) {
-        //             setHasData(false)
-        //             console.log('No data found')
-        //             setErrorMessage('The search engine is currently unavailable. Please try again later.')
-        //             return
-        //         }
-        //         else if (!data.items.length) {
-        //             setHasData(false)
-        //             console.log('No data found')
-        //             setErrorMessage('Sorry, there are no videos matching your search. Please try another search.')
-        //         }
-        //         else {
-        //             setHasData(true)
-        //             setVideosData(data.items)
-        //         }
-        //     })
-    }
-        
-    return (
-        <Context.Provider 
-            value={{searchValue, handleSearchFieldChange, handleSelectChange, videosData, searchForVideos, buttonClass, errorMessage, hasData }}
-        >
-            {children}
-        </Context.Provider>
-    )
+  const buttonClass = searchValue ? "enabled" : "disabled";
+
+  function handleSelectChange(e) {
+    setChannelId(e.target.value);
+  }
+
+  function handleSearchFieldChange(e) {
+    setSearchValue(e.target.value);
+  }
+
+  async function searchForVideos(e) {
+    let conversationStr = "";
+    e.preventDefault();
+    const response = await fetch(URL, {
+      method: "POST",
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "content-type": "text/plain",
+      },
+      body: conversationStr,
+    });
+
+    const data = await response.json();
+    console.log(data);
+    // fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&key=${KEY}&maxResults=10&channelId=${channelId}&q=${searchValue}`)
+    //     .then(res => {
+    //         if (!res.ok) {
+    //             setHasData(false)
+    //             console.log('res.ok === false')
+    //             setErrorMessage('The search engine is currently unavailable. Please try again later.')
+    //         }
+    //         else {
+    //             return res.json()
+    //         }
+    //     })
+    //     .then(data => {
+    //         if (!data) {
+    //             setHasData(false)
+    //             console.log('No data found')
+    //             setErrorMessage('The search engine is currently unavailable. Please try again later.')
+    //             return
+    //         }
+    //         else if (!data.items.length) {
+    //             setHasData(false)
+    //             console.log('No data found')
+    //             setErrorMessage('Sorry, there are no videos matching your search. Please try another search.')
+    //         }
+    //         else {
+    //             setHasData(true)
+    //             setVideosData(data.items)
+    //         }
+    //     })
+  }
+
+  return (
+    <Context.Provider
+      value={{
+        searchValue,
+        handleSearchFieldChange,
+        handleSelectChange,
+        videosData,
+        searchForVideos,
+        buttonClass,
+        errorMessage,
+        hasData,
+      }}
+    >
+      {children}
+    </Context.Provider>
+  );
 }
 
-export {ContextProvider, Context}
+export { ContextProvider, Context };
