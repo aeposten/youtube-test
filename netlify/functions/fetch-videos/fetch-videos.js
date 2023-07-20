65ae8c862c723d9d6cf2b573359a3e0cc92440f2
Amy <86963506+aeposten@users.noreply.github.com>
2023-07-20 08:03:15 -0700

    Updated fetch

--- netlify/functions/fetch-videos/fetch-videos.js
+++ netlify/functions/fetch-videos/fetch-videos.js
@@ -5,9 +5,10 @@ const handler = async (event) => {
     return {
       statusCode: 200,
       body: JSON.stringify({ message: `Hello ${subject}` }),
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
+      headers: {
+        "Access-Control-Allow-Methods": "*",
+        "Access-Control-Allow-Origin": "*",
+      },
     }
   } catch (error) {
     return { statusCode: 500, body: error.toString() }
