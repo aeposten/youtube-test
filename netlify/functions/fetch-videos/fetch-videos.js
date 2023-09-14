fa9d9af365d7e3fb3461adb41e015076c6bbaadc
Amy <86963506+aeposten@users.noreply.github.com>
2023-09-14 08:12:27 -0700

    Updated serverless function

--- netlify/functions/fetch-videos/fetch-videos.js
+++ netlify/functions/fetch-videos/fetch-videos.js
@@ -1,20 +1,8 @@
-const axios = require('axios');
 
 exports.handler = async (event) => {
-  console.log("request")
   try {
+    // Handle CORS preflight request.
     if (event.httpMethod === "OPTIONS") {
-      return {
-        statusCode: 200,
-        headers: {
-          "Access-Control-Allow-Headers": "*",
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-        },
-        body: "",
-      };
-    } else if (event.httpMethod === "POST") {
-      const {name} = JSON.parse(event.body)
       return {
         statusCode: 200,
         headers: {
@@ -22,36 +10,44 @@ exports.handler = async (event) => {
           "Access-Control-Allow-Origin": "*", // Allow from anywhere
           "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
         },
-        body: JSON.stringify({message: "Hello" + name}),
+        body: "",
       };
     }
 
-    const { channelId, searchValue } = JSON.parse(event.body);
+    // Handle POST request.
+    if (event.httpMethod === "POST") {
+      const { channelId, searchValue } = JSON.parse(event.body);
 
-    const API_KEY = "AIzaSyC-0bMXoBYRfR1Gp5k1JCVNy38cgIX_IHk";
+      const API_KEY = "AIzaSyC-0bMXoBYRfR1Gp5k1JCVNy38cgIX_IHk";
 
-    const BASE_URL = "https://www.googleapis.com/youtube/v3/search";
-    const response = await axios.get(BASE_URL, {
-      params: {
-        part: 'snippet',
-        key: API_KEY,
-        maxResults: 10,
-        channelId: channelId,
-        q: searchValue
-      }
-    });
-    
+      const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&key=${API_KEY}&maxResults=10&channelId=${channelId}&q=${searchValue}`;
+
+      const response = await fetch(url);
+      const data = await response.json();
+
+      return {
+        statusCode: 200,
+        headers: {
+          "Access-Control-Allow-Headers": "*",
+          "Access-Control-Allow-Origin": "*", // Allow from anywhere
+          "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+        },
+        body: JSON.stringify(data),
+      };
+    }
+
+    // If the request HTTP method is neither OPTIONS nor POST, return a method not allowed message.
     return {
-      statusCode: 200,
+      statusCode: 405,
       headers: {
         "Access-Control-Allow-Headers": "*",
-        "Access-Control-Allow-Origin": "*", // Allow from anywhere
+        "Access-Control-Allow-Origin": "*", 
         "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
       },
-      body: JSON.stringify(response.data),
+      body: JSON.stringify({ message: "Method not allowed" }),
     };
+
   } catch (error) {
     return { statusCode: 500, body: error.toString() };
   }
 };
-
